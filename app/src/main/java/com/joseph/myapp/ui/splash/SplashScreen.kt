e6a75a3b7e714521e202991daa0c5ed5bd289688
Joseph <jmtorresmultisyscorp.com>
2022-04-06 17:36:44 +0800

    Finished SplashScreen

--- app/src/main/java/com/joseph/myapp/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/joseph/myapp/ui/splash/SplashScreen.kt
@@ -1,15 +1,20 @@
 package com.joseph.myapp.ui.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.joseph.myapp.R
 import com.joseph.myapp.helper.SPLASH_SCREEN_DELAY
 import com.joseph.myapp.navigation.NavDirection
 import com.joseph.myapp.theme.MyTheme
@@ -29,13 +34,20 @@ fun SplashScreen(
 
 @Composable
 fun SplashContent() {
-    Column(
+    Box(
         modifier = Modifier
-            .background(Color.Green)
+            .background(Color.White)
             .fillMaxSize(),
-        verticalArrangement = Arrangement.Bottom,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .wrapContentSize(),
+            painter = painterResource(id = R.drawable.ic_splash_logo),
+            contentDescription = null
+        )
+    }
 }
 
 @Preview(showBackground = true)
