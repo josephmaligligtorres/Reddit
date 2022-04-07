8a28644d799d3905c01b3f0a5bf1e46992134548
Joseph <jmtorresmultisyscorp.com>
2022-04-07 15:38:38 +0800

    Webview back

--- app/src/main/java/com/joseph/myapp/ui/common/WebView.kt
+++ app/src/main/java/com/joseph/myapp/ui/common/WebView.kt
@@ -1,8 +1,16 @@
 package com.joseph.myapp.ui.common
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.view.ViewGroup
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 
@@ -12,15 +20,35 @@ fun WebView(
     modifier: Modifier = Modifier,
     url: String
 ) {
+    var backEnabled by rememberSaveable { mutableStateOf(false) }
+    var webView: WebView? = null
+
     AndroidView(
+        modifier = modifier,
         factory = {
-            android.webkit.WebView(it).apply {
-                webViewClient = WebViewClient()
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = object : WebViewClient() {
+                    override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                        backEnabled = view.canGoBack()
+                    }
+                }
                 settings.javaScriptEnabled = true
                 settings.domStorageEnabled = true
+
                 loadUrl(url)
+                webView = this
             }
         },
-        modifier = modifier
+        update = {
+            webView = it
+        }
     )
+
+    BackHandler(enabled = backEnabled) {
+        webView?.goBack()
+    }
 }
