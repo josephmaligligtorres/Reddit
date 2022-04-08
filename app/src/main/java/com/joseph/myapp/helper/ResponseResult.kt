3a9c6a9ca470696871418295c92ae8e311a3b457
Joseph <jmtorresmultisyscorp.com>
2022-04-08 20:48:11 +0800

    Failed usecase

--- app/src/main/java/com/joseph/myapp/helper/ResponseResult.kt
+++ app/src/main/java/com/joseph/myapp/helper/ResponseResult.kt
@@ -3,11 +3,13 @@ package com.joseph.myapp.helper
 sealed class ResponseResult<out R> {
     data class Success<out T>(val data: T) : ResponseResult<T>()
     data class Error(val message: String) : ResponseResult<Nothing>()
+    data class Failed<out T>(val reason: Enum<*>, val data: T) : ResponseResult<Nothing>()
 
     override fun toString(): String {
         return when (this) {
             is Success<*> -> "Success[data=$data]"
             is Error -> "Error[message=$message]"
+            is Failed<*> -> "Failed[reason=$reason,data=$data]"
         }
     }
 }
